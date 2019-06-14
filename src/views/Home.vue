9e3e9a362749b22e0ba1df46d79fcd2df06b5687
benjaminjkamp <mail@benjaminjkamp.com>
2019-06-14 09:37:21 -0500

    implements CRUD actions on one page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <h1>{{ title }}</h1>
+
+    <h2>Add Movie</h2>
+    Title: <input type="text" v-model="newMovieTitle"><br>
+    Director: <input type="text" v-model="newMovieDirector"><br>
+    Year: <input type="number" v-model="newMovieYear"><br>
+    Plot: <input type="text" v-model="newMoviePlot"><br>
+    
+    <button v-on:click="createMovie()">Add Movie</button>
+
+    <br><br>
+    <div v-for="movie in movies">
+      <br><br>
+      <h2> {{ movie.title }} </h2>
+      <h3> Released in: {{ movie.year }} </h3>
+      
+
+      <div v-if="currentMovie === movie">
+        {{movie}}
+        <h4> Directed by: {{ movie.director }} </h4>
+        <h4> Plot: {{ movie.plot }} </h4>
+        <br>
+        <h3>Update Movie</h3>
+        Title: <input type="text" v-model="movie.title"> <br>
+        Year: <input type="text" v-model="movie.year"> <br>
+        Director: <input type="text" v-model="movie.director"> <br>
+        Plot: <input type="text" v-model="movie.plot"> <br><br>
+        
+
+        <button v-on:click="updateMovie(movie)">Update Movie</button><br><br>
+        <button v-on:click="destroyMovie(movie)">Delete Movie</button><br><br><br>
+        
+      </div>
+      <button v-on:click="showMovie(movie)">Show Details</button>
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      title: "Movies!",
+      movies: [],
+      currentMovie: {},
+      newMovieTitle: "",
+      newMovieDirector: "",
+      newMovieYear: "",
+      newMoviePlot: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/movies").then(response => {
+      this.movies = response.data;
+    })
+  },
+  methods: {
+    showMovie: function(movie){
+      if (this.currentMovie === movie){
+        this.currentMovie = {};
+      } else {
+        this.currentMovie = movie;
+      }
+      
+    },
+    createMovie: function(){
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+        director: this.newMovieDirector
+      };
+
+      axios.post("/api/movies", params).then(response => {
+        this.movies.push(response.data);
+        this.newMovieTitle = "";
+        this.newMovieYear = "";
+        this.newMovieDirector = "";
+        this.newMoviePlot = "";
+        
+      })
+    },
+    updateMovie: function(movie){
+      var params = {
+        title: movie.title,
+        year: movie.year,
+        plot: movie.plot,
+        director: movie.director
+      };
+
+      axios.patch("/api/movies/" + movie.id, params).then(response => {
+        this.currentMovie = {};
+      })
+    },
+    destroyMovie: function(movie){
+      axios
+        .delete("/api/movies/" + movie.id)
+        .then(response => {
+          var index = this.movies.indexOf(movie);
+          this.movies.splice(index, 1);
+      })
+    }
   }
-}
+};
 </script>
